0c493c984c4f5e3ffbe4b9bb3d8f5706795c2bb5
Beryl B <beriebaldwin@gmail.com>
2022-06-15 17:53:43 -0400

    david pokemon update

--- data/pokemon-data.js
+++ data/pokemon-data.js
@@ -1,6 +1,6 @@
 const pokemons = [
   {instructor: 'Ben', mainPokemon: 'Persian', img: './assets/persian.png', specialty: 'electric types', _id: 127664},
-  {instructor: 'David', mainPokemon: 'Stufful', img: './assets/stufful.png', specialty: 'water types', _id: 189604},
+  {instructor: 'David', mainPokemon: 'Ponyta', img: './assets/ponyta.png', specialty: 'water types', _id: 189604},
   {instructor: 'Joe', mainPokemon: 'Clefairy', img: './assets/clefairy.png', _id: 157604},
   {instructor: 'Ian', mainPokemon: 'Minun', img: './assets/minun.png', specialty: 'bug types', _id: 127904},
   {instructor: 'Hunter', mainPokemon: 'Vigoroth', img: './assets/vigoroth.png', specialty: 'ground types', _id: 139608},
